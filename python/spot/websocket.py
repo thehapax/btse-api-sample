265f691ac62c33098e213dc4d03532e0c0d7ff70
thehapax <39811582+thehapax@users.noreply.github.com>
2020-09-21 19:39:49 -0700

    add print statements

--- python/spot/websocket.py
+++ python/spot/websocket.py
@@ -11,8 +11,8 @@ from PeriodicChecker import PeriodicChecker
 # works on testnet and production
 
 # ping checker for keep alive with websocket, aka heartbeat
-ping_checker = PeriodicChecker(period_ms = 30 * 1000)
-
+#ping_checker = PeriodicChecker(period_ms = 30 * 1000)
+ping_checker = PeriodicChecker(period_ms = 8 * 1000)
 
 async def connect_forever():
     path = '/spotWS'
@@ -20,7 +20,9 @@ async def connect_forever():
     
     async with websockets.connect(url) as websocket:
         # Authentication
-        auth_payload = json.dumps(gen_auth(keypair['API-KEY'], keypair['API-PASSPHRASE']))
+        auth = gen_auth(keypair['API-KEY'], keypair['API-PASSPHRASE'])
+        print("***** GEN AUTH: *****" + str(auth))
+        auth_payload = json.dumps(auth)
         await websocket.send(auth_payload)
 
         # Subscription
@@ -49,13 +51,19 @@ async def connect_forever():
         await websocket.send(json.dumps(payload))
         '''
         
+        MESSAGE_TIMEOUT = 30.0
+        PING_TIMEOUT = 10.0
 
         while True:
-            msg = await websocket.recv()
-            print(msg)
+            raw_msg_str: str = await websocket.recv()
+#            raw_msg_str: str = await asyncio.wait_for(websocket.recv(), timeout=MESSAGE_TIMEOUT)
+            print("RECEIVED: " + raw_msg_str)
+            #msg = await websocket.recv()
+            #print(msg)
             if ping_checker.check():
-                print(" sending a ping response of 2" )
-                await websocket.send("2")
+                payload = {"op": "ping"}
+                print("==== Keep Alive HEART BEAT === sending a ping: " + str(payload))
+                await websocket.send(json.dumps(payload))
 
 
 asyncio.get_event_loop().run_until_complete(connect_forever())
