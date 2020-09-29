c3219b4af86c2b0966db8447b05f737bfd8b3819
thehapax <39811582+thehapax@users.noreply.github.com>
2020-09-29 15:32:40 -0700

    add examples

--- python/spot/get_market.py
+++ python/spot/get_market.py
@@ -18,3 +18,41 @@ r = requests.get(BTSE_Endpoint+'/api/v3.2/market_summary', params={
 }, headers = headers)
 
 pp.pprint(r.json())
+
+'''
+example: 
+[   {   'active': True,
+        'availableSettlement': None,
+        'base': 'BTC',
+        'closeTime': 0,
+        'contractEnd': 0,
+        'contractSize': 0.0,
+        'contractStart': 0,
+        'fundingRate': 0.0,
+        'futures': False,
+        'high24Hr': 10758.0,
+        'highestBid': 10757.5,
+        'inactiveTime': 0,
+        'last': 10758.0,
+        'low24Hr': 10741.5,
+        'lowestAsk': 10758.0,
+        'maxOrderSize': 2000.0,
+        'maxPosition': 0,
+        'maxRiskLimit': 0,
+        'minOrderSize': 0.001,
+        'minPriceIncrement': 0.5,
+        'minRiskLimit': 0,
+        'minSizeIncrement': 0.001,
+        'minValidPrice': 0.5,
+        'openInterest': 0.0,
+        'openInterestUSD': 0.0,
+        'openTime': 0,
+        'percentageChange': 0.00464792,
+        'quote': 'USD',
+        'size': 0.766,
+        'startMatching': 0,
+        'symbol': 'BTC-USD',
+        'timeBasedContract': False,
+        'volume': 8240.5455}]
+
+'''
--- python/spot/wallet.py
+++ python/spot/wallet.py
@@ -8,8 +8,10 @@ from btseauth_spot import make_headers, BTSE_Endpoint
 path = '/api/v3.2/user/wallet'
 btse_test_url ='https://testapi.btse.io/spot/api/v3.2/user/wallet'
 
+params = {'currency': 'USD'} 
+
 headers=make_headers(path, '')
-params ={}
+#params ={}
 
 r = requests.get(
     btse_test_url,
@@ -21,8 +23,56 @@ print(r.json())
 
 
 '''
+sample response:  - returns balances of all wallets, selective params don't seem to make a difference
 
-r = requests.get('https://testapi.btse.io/spot/api/v3.2/user/wallet', 
-    params={}, headers = headers)
+nonce:1601362910720
+<Response [200]>
+[{'currency': 'USD', 'total': 9773.219, 'available': 9759.199}, 
+{'currency': 'EUR', 'total': 8000.0, 'available': 8000.0}, 
+{'currency': 'GBP', 'total': 7500.0, 'available': 7500.0}, 
+{'currency': 'HKD', 'total': 70000.0, 'available': 70000.0},
+ {'currency': 'SGD', 'total': 13000.0, 'available': 13000.0}, 
+ {'currency': 'MYR', 'total': 0.0, 'available': 0.0}, 
+ {'currency': 'CNY', 'total': 0.0, 'available': 0.0},
+  {'currency': 'IDR', 'total': 0.0, 'available': 0.0}, 
+  {'currency': 'THB', 'total': 0.0, 'available': 0.0}, 
+  {'currency': 'JPY', 'total': 0.0, 'available': 0.0}, 
+  {'currency': 'AUD', 'total': 0.0, 'available': 0.0}, 
+  {'currency': 'AED', 'total': 0.0, 'available': 0.0}, 
+  {'currency': 'CAD', 'total': 0.0, 'available': 0.0}, 
+  {'currency': 'VND', 'total': 0.0, 'available': 0.0}, 
+  {'currency': 'INR', 'total': 0.0, 'available': 0.0}, 
+  {'currency': 'PHP', 'total': 0.0, 'available': 0.0}, 
+  {'currency': 'CHF', 'total': 0.0, 'available': 0.0},
+  {'currency': 'USDT', 'total': 10000.0, 'available': 10000.0},
+   {'currency': 'BTC', 'total': 15.605787, 'available': 15.605787}, 
+   {'currency': 'ETH', 'total': 70.0, 'available': 70.0}, 
+   {'currency': 'LTC', 'total': 100.0, 'available': 100.0}, 
+   {'currency': 'TUSD', 'total': 50000.0, 'available': 50000.0}, 
+   {'currency': 'USDC', 'total': 50181.767675, 'available': 50181.767675},
+    {'currency': 'FEE', 'total': 0.0, 'available': 0.0}, 
+    {'currency': 'BNS', 'total': 0.0, 'available': 0.0}, 
+    {'currency': 'XMR', 'total': 0.0, 'available': 0.0}, 
+    {'currency': 'DC', 'total': 0.0, 'available': 0.0}, 
+    {'currency': 'BTSE', 'total': 0.0, 'available': 0.0},
+     {'currency': 'XAUT', 'total': 0.0, 'available': 0.0}, 
+     {'currency': 'BCB', 'total': 0.0, 'available': 0.0}, 
+     {'currency': 'XRP', 'total': 0.0, 'available': 0.0},
+      {'currency': 'TR_USDT', 'total': 0.0, 'available': 0.0},
+       {'currency': 'TRYB', 'total': 0.0, 'available': 0.0}, 
+       {'currency': 'LEO', 'total': 0.0, 'available': 0.0}, 
+       {'currency': 'TRX', 'total': 0.0, 'available': 0.0},
+        {'currency': 'STAKE', 'total': 0.0, 'available': 0.0}, 
+        {'currency': 'PHNX', 'total': 0.0, 'available': 0.0}, 
+        {'currency': 'FRM', 'total': 0.0, 'available': 0.0}, 
+        {'currency': 'HXRO', 'total': 0.0, 'available': 0.0},
+         {'currency': 'NZD', 'total': 0.0, 'available': 0.0},
+          {'currency': 'RUB', 'total': 0.0, 'available': 0.0}, 
+          {'currency': 'ZAR', 'total': 0.0, 'available': 0.0}, 
+          {'currency': 'UNI', 'total': 0.0, 'available': 0.0}, 
+          {'currency': 'CRV', 'total': 0.0, 'available': 0.0}, 
+          {'currency': 'COMP', 'total': 0.0, 'available': 0.0}, 
+          {'currency': 'SWRV', 'total': 0.0, 'available': 0.0}, 
+          {'currency': 'BRZ', 'total': 0.0, 'available': 0.0}]
 
 '''
